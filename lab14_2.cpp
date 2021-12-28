b3e33d7cf95100231380e846e5c0e6a6e9cb7e7c
naruemonlarpuangkom <narumon.02545@gmail.com>
2021-12-28 20:26:23 +0700

    14.2 finish

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,26 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+    cout << "------------------------------------------------------------------------\n" ;
+    for (int q = 0 ; q < N ; q++){
+        cout << "|" ;
+        for (int w = 0 ; w < M ; w++){
+        if (image[q][w] == 0) cout << " " ;
+        if (image[q][w] == 1) cout << "*" ;
+        }
+        cout << "|" << "\n" ;
+    }
+    cout << "------------------------------------------------------------------------\n" ;
+}
+
+void updateImage (bool image[][M],int z,int x,int y){
+    for (int i = 0 ; i < N ; i++){
+        for (int j = 0 ; j < M ; j++){
+            if (sqrt(pow(i-x,2)+pow(j-y,2))+1 <= z){
+                image[i][j] = 1 ;
+            }
+        }
+    }
+} 
